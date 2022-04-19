dc3f2e7d4fa60d8dc81d8768ba258027c5677e3c
engin.karatas@dvu.com.tr <43602725+EnginKARATAS@users.noreply.github.com>
2022-04-19 18:44:39 +0300

    assignment done!

--- www/js/index.js
+++ www/js/index.js
@@ -19,11 +19,63 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+  // Cordova is now initialized. Have fun!
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+  let doc = window.document;
+  let pouch_cordova_element = (doc.createElement("h4").innerText =
+    "Cordova 🖤 Mobil Programlama!");
+  doc.getElementById("pouch_cordova_title").append(pouch_cordova_element);
+
+  var db = new PouchDB("userapp", { adapter: "cordova-sqlite" });
+
+  doc = {
+    _id: "Bozok",
+    title: "Bozok Universitesi",
+    lecture: "Cordova 🖤 Mobil Programlama Dersi!",
+    teacher: "Tolga Hayıt",
+    date: "18.04.2021",
+    version: "Version 1.0.0",
+  };
+
+  db.put(doc)
+    .then((res) => {
+      console.log("Document inserted OK");
+      console.log("res: ", res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  db.get("Bozok")
+    .then(function (doc) {
+      console.log("🚀 ~ file: index.js ~ line 54 ~ doc", doc);
+      let docum = window.document;
+      let title = (docum.createElement("h4").innerText = doc.title);
+      let lecture = (docum.createElement("h4").innerText = doc.lecture);
+      let teacher = (docum.createElement("h4").innerText = doc.teacher);
+      let date = (docum.createElement("h4").innerText = doc.date);
+      let version = (docum.createElement("h4").innerText = doc.version);
+      let element_arr = [title, lecture, teacher, date, version];
+
+      element_arr.forEach((item) => {
+        console.log(
+          "🚀 ~ file: index.js ~ line 63 ~ element_arr.forEach ~ item",
+          item
+        );
+        let pouch_cordova_ul = document.getElementById("pouch_cordova_ul");
+        let li = document.createElement("li");
+        let txt = document.createTextNode(item);
+        li.appendChild(txt);
+        pouch_cordova_ul.appendChild(li);
+      });
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+
+  console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
+  document.getElementById("deviceready").classList.add("ready");
 }
